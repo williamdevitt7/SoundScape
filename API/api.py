0cb68512ff8c56fb7a5bdf07040089d9de476b3f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-25 20:52:21 -0400

    A few more additions/tests to the python backend web api

--- API/api.py
+++ API/api.py
@@ -1,2 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response
 import pandas as pd
+import pymysql, sqlalchemy, os
+
+# Create the application instance
+app = Flask(__name__)
+
+# Documentation
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+# Add user
+@app.route('/users/add', methods=['GET'])
+def add_user():
+    # Create connection to database
+    conn = pymysql.connect(host='18.204.82.238', user='AppUser', password='@LymanOW!', db='soundscapedb')
+
+    # Create cursor
+    cursor = conn.cursor()
+
+    # Check for valid arguments passing
+    if 'username' in request.args.keys() and 'password' in request.args.keys():
+        # Retrieve args
+        username = request.args['username']
+        password = request.args['password']
+
+        # Insert new user
+        cursor.execute('INSERT INTO users VALUES (`' + username + '`, `' + password + '`)')
+    # If argument list is invalid respond as an error
+    else:
+        make_response('Invalid arguments', 406)
+
+    # Produce displayable data
+    cursor.execute('SELECT * from users')
+    ret = cursor.fetchall()
+
+    # Close connection
+    conn.close()
+
+    return jsonify(ret)
+
+# Run the application
+app.run(debug=True)

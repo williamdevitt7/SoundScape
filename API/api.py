f5a792c3b4c79039a04d4891dfea3e382246a0a9
BuildTools <unconfigured@null.spigotmc.org>
2020-06-09 23:14:07 -0400

    Changed all requests to POST and added login. Doc page still not quite fully up to date, will do soon

--- API/api.py
+++ API/api.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, jsonify, make_response
 import pandas as pd
 from pandas import DataFrame
-from sqlalchemy import create_engine
-import pymysql, os
+import pymysql
 
 # Create the application instance
 app = Flask(__name__)
+
+# RESPONSE STATUS CODE DICTIONARY
+status_codes = {
+    'OK': 200,
+    'invalid_admin': 205,
+    'invalid_args': 206,
+    'username_exists': 207,
+    'no_username': 208,
+    'no_password': 209,
+    'bad_login': 210
+}
 
 # Documentation
 @app.route('/', methods=['GET'])
@@ -13,8 +23,15 @@ def index():
     return render_template('index.html')
 
 # View Users
-@app.route('/users', methods=['GET'])
+@app.route('/users', methods=['POST'])
 def users():
+    # Secure access
+    with open("admin.txt") as f:
+        if 'admin' not in request.args.keys() or request.args.get('admin') != f.readline().strip():
+            response = make_response('Incorrect admin password')
+            response.status_code = status_codes['invalid_admin']
+            return response
+
     # Create connection to database
     conn = pymysql.connect(host='18.204.82.238', user='AppUser', password='@LymanOW!', db='soundscapedb')
 
@@ -31,8 +48,15 @@ def users():
     return jsonify(ret)
 
 # Add user
-@app.route('/users/add', methods=['GET'])
+@app.route('/users/add,', methods=['POST'])
 def add_user():
+    # Secure access
+    with open("admin.txt") as f:
+        if 'admin' not in request.args.keys() or request.args.get('admin') != f.readline().strip():
+            response = make_response('Incorrect admin password')
+            response.status_code = status_codes['invalid_admin']
+            return response
+
     # Create connection to database
     conn = pymysql.connect(host='18.204.82.238', user='AppUser', password='@LymanOW!', db='soundscapedb')
 
@@ -51,7 +75,7 @@ def add_user():
         for name in users_df["username"]:
             if username == name:
                 response = make_response('User with that username already exists')
-                response.status_code = 207
+                response.status_code = status_codes['username_exists']
                 conn.close()
                 return response
 
@@ -64,7 +88,7 @@ def add_user():
     # If argument list is invalid respond as an error
     else:
         response = make_response('Invalid arguments')
-        response.status_code = 206
+        response.status_code = status_codes['invalid_args']
         conn.close()
         return response
 
@@ -78,8 +102,15 @@ def add_user():
     return jsonify(ret)
 
 # Delete User (probably only used in a backend sense)
-@app.route('/users/delete', methods=['GET'])
+@app.route('/users/delete', methods=['POST'])
 def delete_user():
+    # Secure access
+    with open("admin.txt") as f:
+        if 'admin' not in request.args.keys() or request.args.get('admin') != f.readline().strip():
+            response = make_response('Incorrect admin password')
+            response.status_code = status_codes['invalid_admin']
+            return response
+
     # Create connection to database
     conn = pymysql.connect(host='18.204.82.238', user='AppUser', password='@LymanOW!', db='soundscapedb')
 
@@ -89,7 +120,7 @@ def delete_user():
     # Check for valid parameters
     if "username" not in request.args.keys():
         response = make_response('There was no username parameter given')
-        response.status_code = 208 # Username not passed
+        response.status_code = status_codes['no_username']
         conn.close()
         return response
 
@@ -110,6 +141,58 @@ def delete_user():
     conn.close()
 
     return jsonify(ret)
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Secure access
+    with open("admin.txt") as f:
+        if 'admin' not in request.args.keys() or request.args.get('admin') != f.readline().strip():
+            response = make_response('Incorrect admin password')
+            response.status_code = status_codes['invalid_admin']
+            return response
+
+        # Create connection to database
+        conn = pymysql.connect(host='18.204.82.238', user='AppUser', password='@LymanOW!', db='soundscapedb')
+
+        # Create cursor
+        cursor = conn.cursor()
+
+        # Check for valid parameters
+        username = ''
+        password = ''
+
+        if 'username' in request.args.keys():
+            username = request.args.get('username')
+
+        else:
+            response = make_response('There was no username parameter given')
+            response.status_code = status_codes['no_username']
+            conn.close()
+            return response
+
+        if 'password' in request.args.keys():
+            password = request.args.get('password')
+
+        else:
+            response = make_response('There was no password parameter given')
+            response.status_code = status_codes['no_password']
+            conn.close()
+            return response
+
+        # Query database for user information and determine in login successful
+        user = DataFrame(data=pd.read_sql("SELECT * FROM users WHERE `username`='%s' AND `password`='%s'" % (username, password), conn), index=None)
+
+        if user.empty:
+            response = make_response('Login credentials incorrect')
+            response.status_code = status_codes['bad_login']
+            conn.close()
+            return response
+
+        # Close connection
+        conn.close()
+
+        # Return user's information
+        return jsonify(user.to_json(orient='records')) # TODO BUG WHERE THERE ARE ESCAPE CHARACTERS IN THE JSON
 
 # Run the application
 app.run(host='0.0.0.0')
